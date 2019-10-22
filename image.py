fb0abda4118bb363e25b734acf65694c14424b31
Saurabh Mathur <saurabhmathur96@gmail.com>
2019-10-21 21:33:30 -0400

    Create image.py

---
+++ image.py
@@ -0,0 +1,19 @@
+def read(filepath):
+  ''' reads an image from given filepath and returns a np.array of pixel values '''
+  pass
+
+def to_grayscale(img):
+  ''' converts an rgb image to grayscale '''
+  pass
+
+def orb_features(img):
+  ''' returns the orb features of a grayscale image '''
+  pass
+
+def match(features1, features2, threshold):
+  ''' matches orb features and returns a list of matches  '''
+  pass
+  
+def distance(features1, features2):
+  ''' computes the distance between two images in orb feature space '''
+  pass
